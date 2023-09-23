d9614c8946bec77ae9e2a806f78239b927d380cc
Breno Machado <brenomachado0520@gmail.com>
2023-09-22 22:31:58 -0300

    Atualização do serviço

--- js/script.js
+++ js/script.js
@@ -14,20 +14,29 @@ function generatePDF() {
 }
 
 function preparatePDF() {
+    // Porção
+    document.getElementById('porcao').innerHTML = document.getElementById('floatingPorcao').value ? document.getElementById('floatingPorcao').value + " " + document.getElementById('optionPorcao').value : "200" + document.getElementById('optionPorcao').value;
+
     // Calorias
     document.getElementById('calorias').innerHTML = document.getElementById('floatingCalorias').value ? document.getElementById('floatingCalorias').value + " " + document.getElementById('optionCalorias').value : "0" + " " + document.getElementById('optionCalorias').value;
 
     // Carboidratos
     document.getElementById('carboidratos').innerHTML = document.getElementById('floatingCarboidratos').value ? document.getElementById('floatingCarboidratos').value + " " + document.getElementById('optionCarboidratos').value : "0" + " " + document.getElementById('optionCarboidratos').value;
 
+    // Acucares Totais
+    document.getElementById('acucarTotal').innerHTML = document.getElementById('floatingAcucarTotal').value ? document.getElementById('floatingAcucarTotal').value + " " + document.getElementById('optionAcucarTotal').value : "0" + " " + document.getElementById('optionAcucarTotal').value;
+
+    // Acucares adicionados
+    document.getElementById('acucar').innerHTML = document.getElementById('floatingAcucar').value ? document.getElementById('floatingAcucar').value + " " + document.getElementById('optionAcucar').value : "0" + " " + document.getElementById('optionAcucar').value;
+
     // Proteínas
     document.getElementById('proteinas').innerHTML = document.getElementById('floatingProteinas').value ? document.getElementById('floatingProteinas').value + " " + document.getElementById('optionProteinas').value : "0" + " " + document.getElementById('optionProteinas').value;
 
+    // Gorduras Totais
+    document.getElementById('gordTotal').innerHTML = document.getElementById('floatingGordTotal').value ? document.getElementById('floatingGordTotal').value + " " + document.getElementById('optionGordTotal').value : "0" + " " + document.getElementById('optionGordTotal').value;
+
     // Gorduras Saturadas
     document.getElementById('gordSaturadas').innerHTML = document.getElementById('floatingGordSaturadas').value ? document.getElementById('floatingGordSaturadas').value + " " + document.getElementById('optionGordSaturadas').value : "0" + " " + document.getElementById('optionGordSaturadas').value;
-
-    // Gorduras Insaturadas
-    document.getElementById('gordInsaturadas').innerHTML = document.getElementById('floatingGordInsaturadas').value ? document.getElementById('floatingGordInsaturadas').value + " " + document.getElementById('optionGordInsaturadas').value : "0" + " " + document.getElementById('optionGordInsaturadas').value;
 
     // Gorduras Trans
     document.getElementById('gordTrans').innerHTML = document.getElementById('floatingGordTrans').value ? document.getElementById('floatingGordTrans').value + " " + document.getElementById('optionGordTrans').value : "0" + " " + document.getElementById('optionGordTrans').value;
@@ -35,14 +44,8 @@ function preparatePDF() {
     // Fibras
     document.getElementById('fibras').innerHTML = document.getElementById('floatingFibras').value ? document.getElementById('floatingFibras').value + " " + document.getElementById('optionFibras').value : "0" + " " + document.getElementById('optionFibras').value;
 
-    // Acucar
-    document.getElementById('acucar').innerHTML = document.getElementById('floatingAcucar').value ? document.getElementById('floatingAcucar').value + " " + document.getElementById('optionAcucar').value : "0" + " " + document.getElementById('optionAcucar').value;
-
     // Sódio
     document.getElementById('sodio').innerHTML = document.getElementById('floatingSodio').value ? document.getElementById('floatingSodio').value + " " + document.getElementById('optionSodio').value : "0" + " " + document.getElementById('optionSodio').value;
-
-    // Colesterol
-    document.getElementById('colesterol').innerHTML = document.getElementById('floatingColesterol').value ? document.getElementById('floatingColesterol').value + " " + document.getElementById('optionColesterol').value : "0" + " " + document.getElementById('optionColesterol').value;
 
     // Email
     document.getElementById('email').innerHTML = document.getElementById('floatingEmail').value ? document.getElementById('floatingEmail').value : "minhaempresa@exemplo.com";
